f965bee389d1075f3cd7efd949213d437964225e
Nacho García <hello@nachogarcia.dev>
2022-04-20 16:24:29 +0200

    Add unit tests
    Signed-off-by: Nacho García <hello@nachogarcia.dev>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+describe('SecondTestComponent', () => {
+    it('should create', async() => {
+        await render(AppComponent);
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'tic-tac-toe-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tic-tac-toe-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tic-tac-toe-angular app is running!');
-  });
+        expect(screen.getByText('tic-tac-toe-angular app is running!'));
+    });
 });
